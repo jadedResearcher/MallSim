9987b9f4cbf85c5abca440017e62ed55fe64e391
Jennifer Reynolds <jadedResearcher@gmail.com>
2025-12-20 23:00:12 -0500

    hopy shit it works

--- scripts/custom_utils.js
+++ scripts/custom_utils.js
@@ -7,6 +7,14 @@ whoops
 single function file
 probably should move it back to the utils file but like
 i kinda like the history of it
+
+and then as of 12/20/25 i added so so much to this file because i wanted 
+to do a sECOND classpect system layered on teh first
+so i could use the guides sprites
+
+in true zampanio style instead of getting rid of the first layer
+i just papered over it
+lol
 */
 
 
@@ -111,18 +119,77 @@ const setSpritesForParty = (rand, party) => {
     const base_url = "http://farragofiction.com/CatalystsBathroomSim/audio_utils/weird_sounds/weird_video/WeirdGifs/GuideOfHuntersAndHuntedGodtierSprites/"
 
     //keyed by player title
-    const ratings = {}
+    //we treat aspect a little more seriously than class
+    //because duplicates are more visible
+    //these are MOSTLY for icons on the map
+    const aspect_ratings = {}
+    const class_ratings = {}
     //add up aspect and class values, pick your highest for each
     for (let player of party) {
-        //player.sprite_index = rand.getRandomNumberBetween(0, 15);
+        aspect_ratings[player.title] = bestRatedAspectsForThemes(player.theme_keys);
+        class_ratings[player.title] = bestRatedClassesForThemes(player.theme_keys);
     }
+    //okay now we know what each player would be MOST suited for.
+    //we need to sort them by how many they got for aspects (don't care as much about classes)
+    let sorted_player_list = [...party];//make copy
+    //users.sort((a, b) => a.age - b.age);
+    sorted_player_list = sorted_player_list.sort((a, b) => {
+        return aspect_ratings[a.title].length - aspect_ratings[b.title].length;
+    });
+    //this is so nostalgic, i remember doing something similar for sburbsim
+    let chosen_aspects = [];
+    let chosen_classes = [];
+
+    //because we sorted so the people with the least options go first
+    //they get dibs
+    //when they take things out of the pool
+    //hopefully people with more choices still have things they can do
+    //but worst case they fall back to just picking something unrelated to their themes
+    //yeah i could go for secondary themes but this is already way more complicated than i thought i was gonna do lol
+    //not very zampanio of me but... i liked the guides sprites so much i wanted them to MEAN something
+    for (let player of sorted_player_list) {
+        let aspect_possibilities = aspect_ratings[player.title].filter((aspect) => !chosen_aspects.includes(aspect));
+        if (aspect_possibilities.length === 0) {
+            aspect_possibilities = Object.keys(aspect_mapping).filter((aspect) => !!chosen_aspects.includes(aspect))
+        }
+        if (aspect_possibilities.length === 0) {//fuck it, there must be somehow more than 12 players or whatever. let whatever happen
+            aspect_possibilities = Object.keys(aspect_mapping);
+        }
+        player.sprite_aspect = rand.pickFrom(aspect_possibilities)
+
+        let class_possibilities = class_ratings[player.title].filter((classname) => !chosen_classes.includes(classname));
+        if (class_possibilities.length === 0) {
+            class_possibilities = Object.keys(class_mapping).filter((classname) => !!chosen_classes.includes(classname))
+        }
+        if (class_possibilities.length === 0) {//fuck it, there must be somehow more than 12 players or whatever. let whatever happen
+            class_possibilities = Object.keys(class_mapping);
+        }
+        player.sprite_class = rand.pickFrom(class_possibilities);
+
+        chosen_aspects.push(player.sprite_aspect);
+        chosen_classes.push(player.sprite_class);
+
+    }
+
 }
 
-//figure out what key has the highest value, then return a list of all keys with that value
+//rating is key,value pairs like blood:4, breath: 1
+//figure out what key has the highest value, 
+// then return a list of all keys with that value
 const pareDownRatingToJustBest = (rating) => {
-    const ret = [];
     //JR NOTE: todo
-    return ret;
+    //key is the rating, object is the list of things with that rating
+    let highest_value_so_far;
+    let highest_values_associated_strings = [];
+    for (let [key, value] of Object.entries(rating)) {
+        if (!highest_value_so_far || value > highest_value_so_far) {
+            highest_value_so_far = value;
+            highest_values_associated_strings = [key];
+        } else if (value === highest_value_so_far) {
+            highest_values_associated_strings.push(key)
+        }
+    }
+    return highest_values_associated_strings;
 }
 
 const bestRatedAspectsForThemes = (theme_keys) => {
