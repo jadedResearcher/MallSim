18085778bcda82450f07dc5b8b9787398b0134e0
Jennifer Reynolds <jadedResearcher@gmail.com>
2025-12-20 22:00:27 -0500

    refactor

--- scripts/custom_utils.js
+++ scripts/custom_utils.js
@@ -118,6 +118,22 @@ const setSpritesForParty = (rand, party) => {
     }
 }
 
+//figure out what key has the highest value, then return a list of all keys with that value
+const pareDownRatingToJustBest = (rating) => {
+    const ret = [];
+    //JR NOTE: todo
+    return ret;
+}
+
+const bestRatedAspectsForThemes = (theme_keys) => {
+    return pareDownRatingToJustBest(calculateAspectRatingsForThemes(theme_keys))
+
+}
+
+const bestRatedClassesForThemes = (theme_keys) => {
+    return pareDownRatingToJustBest(calculateClassRatingsForThemes(theme_keys))
+}
+
 const calculateAspectRatingsForThemes = (theme_keys) => {
     const ret = {};
     for (let [key, value] of Object.entries(aspect_mapping)) {
@@ -164,13 +180,13 @@ const debugRenderSpriteForEntitty = () => {
     const list = rawList.split(",");
 
     console.log("JR NOTE: list is", list)
-    for (let i = 0; i <= 15; i++) {
+    for (let classname of Object.keys(class_mapping)) {
         const parent = createElementWithClassAndParent("div", body);
         parent.style.cssText = `display: flex; flex-wrap: wrap; gap: 3px;`;
 
         for (let item of list) {
-            const sprite = renderSpriteForEntity(parent, { sprite_url: base_url + item + ".png", sprite_index: i })
-            sprite.title = i;
+            const sprite = renderSpriteForEntity(parent, { sprite_aspect: item, sprite_class: classname })
+            sprite.title = classname;
             sprite.style.border = "1px solid black";
         }
     }
@@ -179,30 +195,33 @@ const debugRenderSpriteForEntitty = () => {
 
 const renderSpriteForEntity = (parent, entity) => {
     console.log("JR NOTE: rendering", entity)
+    const base_url = "http://farragofiction.com/CatalystsBathroomSim/audio_utils/weird_sounds/weird_video/WeirdGifs/GuideOfHuntersAndHuntedGodtierSprites/"
+
     const sprite = createElementWithClassAndParent("div", parent, "entity-sprite");
-    sprite.style.backgroundImage = `url(${entity.sprite_url})`;
+    sprite.style.backgroundImage = `url(${base_url + entity.sprite_aspect}.png)`;
     //entity.sprite_index
+    const class_array = Object.keys(class_mapping)
     //feeling lazy, just gonna map index to x,y coordinates till i discover a pattern
     const hopefully_temporary_mapping = {}
-    hopefully_temporary_mapping[0] = [-19, 0]//good
-    hopefully_temporary_mapping[1] = [-81, 0] ///good
-    hopefully_temporary_mapping[2] = [-160, 0] //good
-    hopefully_temporary_mapping[3] = [-218, 0] //good
-    hopefully_temporary_mapping[4] = [-16, -137] //good
-    hopefully_temporary_mapping[5] = [-85, -144] //good
-    hopefully_temporary_mapping[6] = [-155, -145] //good
-    hopefully_temporary_mapping[7] = [-225, -137] //good
-    hopefully_temporary_mapping[8] = [-7, -280]//good
-    hopefully_temporary_mapping[9] = [-81, -280] //good
-    hopefully_temporary_mapping[10] = [-155, -280]//good
-    hopefully_temporary_mapping[11] = [-226, -280]//good
-    hopefully_temporary_mapping[12] = [-19, -416] //good
-    hopefully_temporary_mapping[13] = [-88, -416] //good
-    hopefully_temporary_mapping[14] = [-154, -416] //good
-    hopefully_temporary_mapping[15] = [-227, -416] //good
+    hopefully_temporary_mapping[class_array[0]] = [-19, 0]//good
+    hopefully_temporary_mapping[class_array[1]] = [-81, 0] ///good
+    hopefully_temporary_mapping[class_array[2]] = [-160, 0] //good
+    hopefully_temporary_mapping[class_array[3]] = [-218, 0] //good
+    hopefully_temporary_mapping[class_array[4]] = [-16, -137] //good
+    hopefully_temporary_mapping[class_array[5]] = [-85, -144] //good
+    hopefully_temporary_mapping[class_array[6]] = [-155, -145] //good
+    hopefully_temporary_mapping[class_array[7]] = [-225, -137] //good
+    hopefully_temporary_mapping[class_array[8]] = [-7, -280]//good
+    hopefully_temporary_mapping[class_array[9]] = [-81, -280] //good
+    hopefully_temporary_mapping[class_array[10]] = [-155, -280]//good
+    hopefully_temporary_mapping[class_array[11]] = [-226, -280]//good
+    hopefully_temporary_mapping[class_array[12]] = [-19, -416] //good
+    hopefully_temporary_mapping[class_array[13]] = [-88, -416] //good
+    hopefully_temporary_mapping[class_array[14]] = [-154, -416] //good
+    hopefully_temporary_mapping[class_array[15]] = [-227, -416] //good
 
-    sprite.style.backgroundPositionX = hopefully_temporary_mapping[entity.sprite_index][0] + "px"
-    sprite.style.backgroundPositionY = hopefully_temporary_mapping[entity.sprite_index][1] + "px"
+    sprite.style.backgroundPositionX = hopefully_temporary_mapping[entity.sprite_class][0] + "px"
+    sprite.style.backgroundPositionY = hopefully_temporary_mapping[entity.sprite_class][1] + "px"
 
     if (entity.wasted) {
         sprite.style.filter = "contrast(100.0)";//they look more real
