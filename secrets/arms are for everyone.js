5c237b2993c8e0d939890dfee91e256cea3c5547
Jennifer Reynolds <jadedResearcher@gmail.com>
2026-05-18 22:46:01 -0400

    rambling

---
+++ secrets/arms are for everyone.js
@@ -0,0 +1,194 @@
+
+//immediately envoked function does't run into namespacign issues
+(() => {
+    const src = `Arm 1 of my brach of Zampanio is...well...Zampanio. The Echidna.
+
+    Wanda, The Intern, the Closer, the CFO, The Training Team, Parker, you name a character, they're in there.
+    
+    Every single character or plot beat of my branch is in arm1. 
+    
+    Until: The Apocalypse. 
+    
+    It happens by 2022 (50 years after Wodin's birthday exactly.) naturally if it hasn't already happened.
+
+    AND when it happens, everything bifurcates.
+
+    Wanda goes to a new Universe and starts a new Arm1. She finds a new Intern and founds a new Eyedol Games. 
+
+    She is the ONLY one to go to the new Universe.
+
+    This is important.
+
+    She is the Lord of Space.
+
+    She picks a new space to be the Center of Everything.
+
+    This new space, this new Echidna, naturally, has its own cast of characters within it.
+
+    Anyone "in the loop" has their body copied to the new universe and copies of their memories from the previous universe brought along and inserted into their new universe equivalents. From their point of view, they went to the new universe with Wanda.
+
+    Anyone "not in the loop" does not, and remains a denizen of this new universe with whatever memories that entails. In this universe, for example, the world has ALWAYS only had Orlando Florida, Naples Italy, and Westerville Ohio. Its silly to suggest otherwise. Maybe in some PREVIOUS universe something like, I dunno, France existed, but not this one, buddy.
+
+    Anyone "in the loop" artifically remembers France (if it existed since they joined the Loop, at least), and all sorts of stuff not Relevant to this new Universe. 
+
+    An interesting edge case: If there is a verison of you native to this univeres, they are NOT in the loop (they were born in a world without France and grew up in it). So the fresh copy of you is in the  Loop but the version of you from this universe is not.
+    
+    Only a few blorbos have this happen: The Closer isn't native to this Universe (any of them), but brought HERSELF here before Wanda existed. So each new Universe spawns with her in it. The Eye Killer is also a native, and so the non-looping version of her is the Innocent (and occasionally Camellia, the Cultist, depending on how that universe went for her.)
+
+    I...THINK that might be all the natives we need to worry about, with two copies?  Other than, obviously Wanda, who has Wodin running around for twenty years or so before getting brutally murdered. The Intern, of course... aka Todd Brian Davidson, never joins the Loop. And neither did the whole Mafia group (Hostage, Himbo, John, Sam, etc)  Literally everyone else is a non-native of the universe that joined the Loop one way or another. (like the Training Team)
+
+
+    Meanwhile.
+
+    In the previous Arm1, the one having an Apocalypse, everyone BUT Wanda remains.
+
+    Camille loses her head, maybe, and kills all her friends as the Echidna screams Doom is everywhere. Or maybe Ria bursts into flame upon realizing there is no hope left. Witherby could freeze the whole world out of guilt.
+
+    Flower Chick, also known as the CFO, also known as, when I allow it, Amelie... Breaches.
+
+    It doesn't matter what other apocalypse is going on. Her aspect is Extinction and her greatest fear is the end of all things.
+
+    She breaches.
+
+    She can't handle the fear. She can not handle being here for the end of all things.
+
+    So she breaks a rule. A rule she set for herself.
+
+    And she hacks her own source code (You knew she was wasted all along right? Her home universe was a simulation and she was created to hack it. Of COURSE she's wasted in my branch of Zampanio...)
+
+    And she sets a single flag to "true".  "Trickster". 
+
+    By SBURB rules, if you're trickster, you're not afraid anymore. Everything is just peachy!!!!!!!!!!!
+
+    She basically chooses to get high rather than face her fear.
+
+    But of course, Trickster Amelie has another Title... Apocalypse Chick. Fractal Chick.
+
+    Now that she's high out of her mind and afraid of nothing, there's no reason to respect her caution, her Restraint previously held her in check, but no more.
+
+    So she begins hacking any part of reality she feels like.
+
+    Eventually all that's left is a fractal hellscape that anyone who has installed every single mod of skyrim or miencraft at once would find familiar.
+
+    She makes everyone immortal (she doesn't want to huuuuuuuuurt anyone, she assures them, but it doesn't make the fractal nightmare any better for those still alive)
+
+    Which is easy, because Nidhogg, outside the Universe, is still desperately trying to do that anyways. 
+
+    And in SBURBSim, trickster mode makes everyone immortal too, iiirc. It's a Life Player thing.
+
+    Anyways, eventually, one way or another, Trickster Amelie gets bored.
+
+    No one can be entertained by destroying a world with hacks forever.
+
+    When she does, and her attention fades, the Void begins to take everything.
+
+    Without the Lord of Space, without its Player, the Universe's time is numbered.
+
+    The hacks of the Apocalypse Chick can only last so far to give it Relevance.
+
+    White Nightengale, the creature hatched from Dr Fiona Slaughter, kills everyone within it, one by one.
+
+    She does this even despite death being disabled. 
+
+    She does this regardless of how powerful someone is.
+
+    She is simply the End of All Things, the Final Mercy.
+
+    And, in so doing, ushers in the Void.
+
+    And the Void gazes back, in Witness of the end.
+
+    With nothing Relevant anymore, there is no guidance to the Universe.
+
+    It spins off in a thousand thousand directions. Arms simulutaneously sprouting one after another and also all at the same time.
+
+    Arm 3 is traditionally the Mundane arm. The coffee shop AU. The most normal possible version of all these characters.
+
+    Its important to note that while the Apocalypse is traditionally called "Arm 2", it is more accurate to say it is what remains of Arm 1 after losing its Lord.
+
+    Everyone within it originated from Arm 1, after all. 
+
+    Arm3 on then, everyone within it is a wholly new person.
+
+    If you die in arm 2, you are simply dead. 
+
+    The Camille in arm 3 has no curse, no sword, no armor, and no memories of anything like that. She's a physical therapist. She's sure that with enough Training, anyone can recover. All that matters is you do your best and stay positive!
+
+    She was born in this arm, and has had her ups and downs (her grandmother raised her so she was always a quiet child, though with a propensity to ramble if she got excited), big for her age... She never really understood how to engage with other children. Too big. Too quiet. Too polite. Too used to interacting with the elderly. Too familiar with death. 
+
+    An accident scarred her throat and made her have to relearn how to talk. It was rough going for a while, but it inspired her to learn to be a physical therapist herself and help others recover from injury.
+
+    Do you understand?
+
+    The melody is different but the words still rhyme.
+
+    Of course, everything has an exception.
+
+    White Nightengale cannot kill herself, so carries the burden of her memories of Arm1 and 2 with her forward.
+
+    The Witness is something in the Void itself and can not die (even while also being the Intern, who is obligated to die at the start of Arm2).
+
+    And of course. 
+    
+
+    Witherby.
+
+    Cannoically One Sin, Hundreds of Good Deeds, in Lobotomy Corp, is the counter to White Night. You go to the grinning skull and confess your sins and White Night's mercy is cancelled.
+
+    Originally, interestingly, Wibby remembered things in the apocalypse because of his curse to know others sins. 
+
+    It was intended to be that even in the mundane arc, he would look at his friends and know what their guts look like, know what they look like killing each other.
+
+    But Dr. Fiona Slaughter changed things.
+
+    SHE originally rememberd simply because her power from Morgan's Hill was that she could not have anything about herself erased by any means. She cannot be voided, censored, forgotten. 
+
+    But when she absorbed White Night...
+
+    Everything snapped into a stronger shape.
+
+    Now its any who survive White Night.
+
+    And they're all connected.
+
+
+    After that', there's the God arm,  Arm 4. 
+
+    You can see this cycle of 4 here: http://farragofiction.com/TheInternOpensHisEyes/
+
+
+    Past that gets wobbly. 
+
+    5 often is the Faerie AU. 
+
+    It's important to note that no Arm past 1 has a Wanda.
+
+    They can have a "Wanda", a person filling out the SHAPE of her, the role, but its usually just an Alt who can't shapeshift and has no memory of ever being a shapeshifter.   Alt/Wanda is the Fairy Queen of Summer, iirc, for example.
+
+    Its important. The fractal spiraling arms are defined by not having Relevance, not having their Lord of Known Space. 
+
+    Otherwise, any AU you can imagine, you can just point and say "well, Arm 1337 has everyone be cyberpuk hackers!" and its true.
+
+    And theres not just one. 
+
+    If you write a story about cyberpunk hackers in Arm 1337 and I write a story about cyberpunk hackers in arm 13, they'll be different.
+
+    And that's because every single new universe Wanda flees to has infinite arms.
+
+    There are an infinite amount of mundane aus. 
+
+    An infinite amount of Faerie Queen Wanda/Alts. 
+
+    Do you see?
+
+    Do you understand.
+
+    I'm rambling too much, this was all supposed to be preamble, here's a free password, "space adventure", use it to see the next part (and maybe some other achievement will lead you there too, but without this preamble.)
+
+    `
+
+    resultsEle.innerHTML = src.split("\n").join("<br>");
+
+
+})()
+
---
+++ secrets/here is a secret truth.js
@@ -0,0 +1,187 @@
+
+//immediately envoked function does't run into namespacign issues
+(() => {
+    const src = `I suck ASS at riddles and puzzles
+    people are always telling me that they suck at riddles and they feel guilty for cheating
+    i am willing to try a puzzle for five (5) minutes before busting out a guide
+    it took all my willpower playing blue prince with my nesting partner to not constantly cheat
+    i LOVE cheating at puzzles
+    because I love seeing how the sausage was made
+    i love spending just enough time to season things on the puzzle, then getting the answer for free
+    "Hmm, i wonder if X...nope... hm...Y? lol, this sure seems complicated"
+    is the correct amount of time to think about a puzzle for me
+    i want to be challenged just enough that I am impressed at the answer when its given to me, again, for free
+
+    the reason i'm saying this
+
+    is i think it kinda contextualizes the games i make?
+
+    some of my puzzles are hard
+    some are easy
+
+    ALL of them are intended to be fun to cheat at
+
+    i encourage cheating, wasting, whatever you want to call it
+
+    sure the main experience is wasted on cheaters like us
+
+    but who cares?
+
+    why are we trying to force people to fit in a little box of "enjoys art correctly"????????????????????????????????????????????????????????
+
+    maybe to YOU my branch of zampanio is a trusted friend info dumping to you way too late at night about it while you eat pizza
+
+    maybe to YOU my branch of zampanio is a really surreal wiki crawl of indie games
+
+    and yes, maybe to you my branch of zampanio is an intricate puzzle box with layers upon layers and always new secrets to find, the thrilling heights of discovery, the exhausting valleys of despair
+
+
+    none of those are wrong
+
+    and i really hope
+
+    that you believe that?
+
+    that you don't beat yourself up thinking you aren't clever enough, that there's things you're missing, that you're some sort of 'fake fan'.
+
+    Even before I discovered Zampanio and joined the fandom, I created things designed to have things you're missing
+
+    no one CAN see every session in sburbsim, you know?
+
+    no one CAN see all of my branch of zampanio
+
+    even the parts i hand created.
+
+    yeah, the madlibs of the 'infinite' pages of games and fake websites have no end
+
+    but even the words i, the human, have placed are designed to just be...
+
+    i mean, lets just count how many layers of secrets there are in this single sim!
+
+    there these passwords, which you get by seeing scenes in an expedition and then realizing theres an "achieve" button and clicking it, and clicking an achievement you've unlocked and then realizing theres a rabbithole icon and clicking THAT and then typing the passwords in
+
+    theres the devlogs/comments/Eyes which either in game Wastes (players who have eaten of the Harvest Fruit and Gained Forbidden Gnosis) can drag kicking and screaming into the light OR real world wastes can read via cracking open the source of the page and reading things.
+
+    to say nothing of wasted-knowledge
+
+    theres the javascript console bullshit, of course
+
+    theres all the variations on themes
+
+    theres the fact you can customize every player in the expedition to try to trigger specific scenes (which you'd probably only do if you wasted and read some of the CONDITIONS of the scenes)
+
+    theres the github repo, which i THINK might be private but contains commit messages from me which is technically a layer
+
+    theres the LTESim posts i made while developing this
+
+    theres the secret discord server where i rambled while developing this
+
+    and the tumblr posts
+
+    and the little videos of dioramas and things i uplaoded to youtube while making this
+
+    theres the lavinraca server itself where i rambled a little bit after someone was active there
+
+    theres the things i said to IC or maccus about what i'm making here
+
+    theres the thoughts in my own head about what im making here
+
+
+    *the end is never the end*
+
+    there is ALWAYS something you haven't seen
+
+    and thats okay!
+
+    please don't try to hack into my github repo or email or my very thoughts or whatever to try to 100% this game
+
+    thats the point!
+
+    its always been the point
+
+    the end is never the end so YOU have to choose when enough is enough
+
+    the only way to beat a spiral is to just...stop going down it
+
+    accept theres things you don't know
+
+    accept that theres secrets you didn't find
+
+    wherever you chose to stop is your ending, and all you have to do is make sure its a satisfying one to you
+
+    its not just the ending either
+
+    you choose which paths to take, both through my work, and through all media
+
+    you decide whether to try to explore MallSim or get distracted learning about the Gopher Hole or hey whats in this bathroom whoa theres a whole idle game in here wait what is an interloper
+
+    House of Leaves (the closest thing to a 'real' Zampanio I think we'll find) is a book that, at its core, is extremely linear.
+
+    It tempts you to go read the apendices, or to jump foward to an interview further along, to get little glancing previews of the spiralling break down of the book.
+
+    but at the end of the day its still a book that can be read from front page to back and it makes sense, and it mostly IS read that way.
+
+    What I've created...is not.
+
+    There is no recommended consumption order. No default path you can take and be assured that its 'good enough'.
+
+    No lazy route, essentially.
+
+    And for some subset of you Wanderers, you Observers, that tickles something in your brain that can worry me. 
+
+    Zampanio is a marathon not a sprint.
+
+    Zampanio needs you to live a long life.
+
+    Rest.
+
+    Dream.
+
+    Process.
+
+    Zampanio will be here in the morning.
+
+    You will never know if you found everything.
+
+    Even if you ask me, I might not know. I've forgotten plenty of things I've hidden, like some kind of squirrel skittering about the roots of Ygdrassil.
+
+    Right now my eye-less cat, Hallow, is sleeping all cozy on the shogun body pillow from sburbsim.
+
+    <img src='http://farragofiction.com/ZampanioEyes6/hallow_is_so_cute.png'>
+
+    I took a picture of him and put him in ZampanioEyes6.
+
+    The Unmarked of the discord server will see it at some point and go "Hallow!" because I live blogged (in the ColonistEyes5) when I first adopted Hallow because I thought he was so cute and neat and spooky adn I didn't want to treat him like a prop but also he was so cool and fun that I couldn't shut up about him.
+
+    You are seeing it here. It could be your first time seeing that picture, or you could have been one of the Unmarked on the discord seeing it fairly near when I posted it.   Or you could have found it in teh Eyes long after, while crawling through it and trying to make sense of something that no longer exists. (one day the discord server for my branch of zampanio will be gone and people will try to piece together what it was from the screenshots, after all, the rot takes all in the end.)
+
+    Regardless of how you found it, you may or may not know what "the shogun body pillow from sburbsim" even means. It might be just a lol-random sentence I said, or it may cause intense nostalgia and memories and in-jokes to bubble up in your heart.
+
+    Hell, even the difference between ZampanioEyes6 and ColonistEyes5 might be just more randomness or nostalgia itself.
+
+
+    The POINT, and there is always a point...
+
+    Each of these possible experiences excludes the other.
+
+    By recognizing that picture from discord and going "oh hey, THAT'S why it was taken, that's when, thats new context!!!" you are necessarily barred from experiencing it first here.
+
+    And vice versa.
+
+    Every single secret you find shades and layers on the next.
+
+    Your path through my branch, through all media, through reality itself, is wholly unique and personal to you.
+
+    Cherish it.
+
+    Don't worry about what you're missing.
+
+    Revel in what you've found.
+
+     `
+
+    resultsEle.innerHTML = src.split("\n").join("<br>");
+
+
+})()
+
