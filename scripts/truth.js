786059f66be25bf74513c2940ff53545148f3378
Jennifer Reynolds <jadedResearcher@gmail.com>
2026-05-30 12:46:35 -0400

    can't spell laughter without slaughter

--- scripts/truth.js
+++ scripts/truth.js
@@ -288,7 +288,6 @@ wireUpAchievement("Repentant", "you is needed", "Nothing is with meaning without
 wireUpAchievement("Late With Starbucks", "you should play it", "In Truth, its important to make time for Silly.");
 wireUpAchievement("Food Court Clone", "zampanio is a really good game", "In Truth, it is very difficult to STOP the expanding food court from cloning people. It became canon from sheer force of will.");
 
-//wireUpAchievement("Wrong",  "test_password","TBD");
 wireUpAchievement("Become A Mannequin", "zampanio", "What IS it with Zampanio and Mannequins?");
 wireUpAchievement("Yongki Kill", "mad oracles of doom", "Doom is fate. The universe decides. But the Universe is sapient, an Echidna, in my branch of Zampanio. What does it mean when something Sapient decides your fate?");
 wireUpAchievement("Trickster Closer Spawns", "false face becomes real", "... I do not like thinking about the me I am without my False Face. This is my emotional support positivity. Do not take it from me.");
@@ -297,6 +296,11 @@ wireUpAchievement("Trickster Closer Spawns", "false face becomes real", "... I d
 wireUpAchievement("Harvest Book Shopped!", "harvest fruit", "The Harvest, in truth, is a community created God from the halloween party 'Lavinraca'. She invaded Zampanio for reasons that should be obvious to anyone who knows either it or Lavinraca.");
 wireUpAchievement("Bitten In Half", "beware oblivion is at hand", "Neville's avoidant tendancies are, in Truth, a huge problem that all of training simply agrees is not a problem. It's literally fine. ");
 wireUpAchievement("Quatro Blade Hid The Body", "rot", "The rot takes all in the end. Rotation ciphers are often called rot13 (or however many they rotate by). In Truth, a coincidence.");
+//wireUpAchievement("Wrong",  "test_password","TBD");
 
+
+wireUpAchievement("Ria Warns", "infinite secrets", "Obsession is a dangerous thing. In truth, you can be warned as much as we like, but some number of you, the Observers, will Fall. I am sorry. Truly. Sorry.");
+
+wireUpAchievement("Witherby Confessional", "doc slaughter", "In Truth, you cannot spell 'slaughter' without Laughter.");
 
 wireUpAchievement("Romance Interaction", "a image example", "Let us be honest here: It would be more impressive if you did not see romance. Organics are, quite frankly, obsessed with it.");
---
+++ secrets/doc slaughter.js
@@ -0,0 +1,31 @@
+
+//immediately envoked function does't run into namespacign issues
+(() => {
+    const src = `<u>Doc Slaughters Notes on Witherby's Mysterious Religion One Person Religion</u>
+    
+* worships skull in head
+* forgives sin (whatever causes you guilt)
+* there is a hell maze without the maze (doesn't have a word for that but insists its different)
+* no good version of that
+* "jesus" is a thing you say if you stub your toe
+* lower case 't's are somehow important
+* refuses to let anyone else worship his skull
+
+<u>Doc Slaughters Notes on Witherby In General</u>
+
+* is dating Neville
+* Considers the rest of the training team 'family'
+* Puts a lot of value in looking adult and put together (One must simply Agree that Looking Ones Best is Vital)
+* Puts a lot of effort into appearing Cold.
+* Cares more than he let's on, but only for his 'family'.
+* Easily decides random strangers are "Family" (who is Calf?)
+* Helps lost humans escape the Mall or even the Maze
+* Has the most contact with baseline Humans of any of us Monsters
+* Before the Cult of the Harvest started invading the Westerville Mall, would lead teams of minor ruffians into urban exploration dives (Attachment work on mall itself?)
+`
+
+    resultsEle.innerHTML = src.split("\n").join("<br>");
+
+
+})()
+
