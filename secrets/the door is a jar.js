4645a1d0f7f72dbe16c1014489a13cebbf240074
Jennifer Reynolds <jadedResearcher@gmail.com>
2026-06-12 20:19:15 -0400

    the words won't come

--- secrets/the door is a jar.js
+++ secrets/the door is a jar.js
@@ -1,12 +1,19 @@
 
 //immediately envoked function does't run into namespacign issues
 (() => {
+
+    const clickAudio = new Audio("http://farragofiction.com/ZampanioSim/audio/web_SoundFX_254286__jagadamba__mechanical-switch.mp3");
+    const musicAudio = new Audio("http://farragofiction.com/ZampanioSim/audio/slide.mp3");
+    musicAudio.volume = 0.25;
+    const doorAudio = new Audio("http://farragofiction.com/ZampanioSim/audio/close_door_1.mp3");
+
+
     //entrance, four rooms and coffin, each with a sacrifice before and hydration after.
-    const mind = true; //will you sacrifice remembering your thoughts
-    const eyes = true; //will you sacrifice remembering your senses
-    const tongue = true; //will you sacrifice remembering your words
-    const arms = true; //will you sacrifice remembering your actions
-    const legs = true; //will you sacrifice remembering your agency
+    let mind = true; //will you sacrifice remembering your thoughts
+    let eyes = true; //will you sacrifice remembering your senses
+    let tongue = true; //will you sacrifice remembering your words
+    let arms = true; //will you sacrifice remembering your actions
+    let legs = true; //will you sacrifice remembering your agency
 
 
 
@@ -20,13 +27,13 @@
         hydration = "This is hydration text"
 
         constructor(text, m, e, t, a, l, h) {
-            this.text = text;
-            this.mind = m;
-            this.eyes = e;
-            this.tongue = t;
-            this.arms = a;
-            this.legs = l;
-            this.hydration = h;
+            this.text ||= text;
+            this.mind ||= m;
+            this.eyes ||= e;
+            this.tongue ||= t;
+            this.arms ||= a;
+            this.legs ||= l;
+            this.hydration ||= h;
         }
     }
 
@@ -128,10 +135,46 @@
     let rand = new SeededRandom(new Date().getTimezoneOffset());
     results.innerHTML = "..."
 
-    let room = createElementWithClassAndParent("div", results, "room");
+    let roomContainer = createElementWithClassAndParent("div", results);
+    musicAudio.loop = true;
+    roomContainer.onclick = () => {
+        clickAudio.play();
+        if (musicAudio.pause) {
+            musicAudio.play();
+        }
+    }
+
+    let room = createElementWithClassAndParent("div", roomContainer, "room");
+    let storyEle = createElementWithClassAndParent("div", roomContainer, "story");
+    storyEle.innerHTML = `You find yourself at the entrance of a poorly constructed maze.
+<br><Br>
+You have 0 gopher gold! `;
+
     let style = createElementWithClassAndParent("style", results);
-    style.innerHTML = `.room{
-    width: 50%;
+    style.innerHTML = `
+
+    button{
+        background: black;
+    color: white;
+    outline: none;
+    border: none;
+    padding: 13px;
+    border-radius: 13px;
+    }
+
+        .story{
+            width: 612px;
+            margin-left:auto;
+            margin-right: auto;
+            margin-top: 31px;
+            border: 5px ridge black;
+            padding: 31px;
+            background-color: rgba(100,100,0,0.5)
+        }
+    
+    
+    .room{
+    width: 612px;
     margin-left: auto;
     margin-right: auto;
     background: black;
@@ -141,6 +184,10 @@
 
     .jar{
         cursor:pointer;
+    }
+
+    .jar:hover{
+        filter: saturate(1.5)
     }
     .jar:nth-child(1 of .jar){
         transform: rotate3d(0, 10, 0, 72deg) scaleX(-1) translate(50%, 10px);
@@ -153,6 +200,8 @@
         .jar:nth-child(3 of .jar){
         transform: rotate3d(0, 10, 0, -72deg) translate(50%, 10px);
     }
+
+    
     `;
 
 
@@ -174,31 +223,122 @@
             alert("JR NOTE: todo coffin time")
         }
 
-        const doorAndStory = () => {
+        const tellStory = (story, sacrifice) => {
+            storyEle.innerHTML = `Very well. Your ${sacrifice} accepted.<br><br>
+            <hr>
+                ${story.text}
+                <br><hr><br>
+                ${mind ? story.mind : "It's hard to think..."}
+                                <br><hr><br>
+                ${eyes ? story.eyes : "Your vision is getting blurry..."}
+                                <br><hr><br>
+
+                ${tongue ? story.tongue : "The words don't seem to come."}
+                                <br><hr><br>
+
+                ${mind ? story.arms : "What's the point of even trying?"}
+                                <br><hr><br>
+
+                ${legs ? story.legs : "It hardly seems worth the effort to move."}
+
+
+
+
+            `;
+        }
+
+        const doorAndStory = (direction) => {
             console.log("JR NOTE: door and story")
             const story = rand.pickFrom(remaingStories);
+            console.log("JR NOTE: story is: ", story)
             const door = createElementWithClassAndParent("img", room, "jar");
             door.src = "http://farragofiction.com/CatalystsBathroomSim/audio_utils/weird_sounds/weird_video/WeirdGifs/door_without_rug.png";
             door.onclick = () => {
-                //console.log("JR NOTE: need to sacrifice something, then show new room, tell the story, then tell the METAL, then hydrate, then let you pick next door")
-                alert("TODO " + story.text);
+                console.log("JR NOTE: need to sacrifice something, then show new room, tell the story, then tell the METAL, then hydrate, then let you pick next door")
+                const rivals = room.querySelectorAll(".jar");
+                for (r of rivals) {
+                    if (r !== door) {
+                        r.style.opacity = 0.1;
+                    }
+                }
+
+                storyEle.innerHTML = `You have chosen the door to the ${direction}.
+                <br><Br>Progress demands sacrifice. What are you willing to sacrifice?`;
+                const buttonContainer = createElementWithClassAndParent("div", storyEle);
+                buttonContainer.style.cssText = `display: flex;
+                justify-content: space-between;
+                margin-top: 13px;`
+                if (mind) {
+                    const button = createElementWithClassAndParent("button", buttonContainer);
+                    button.innerText = "My Mind"
+                    button.onclick = () => {
+                        mind = false;
+                        tellStory(story, "mind is");
+                    }
+                }
+
+                if (eyes) {
+                    const button = createElementWithClassAndParent("button", buttonContainer);
+                    button.innerText = "My Eyes"
+                    button.onclick = () => {
+                        eyes = false;
+                        tellStory(story, "eyes are");
+                    }
+                }
+
+                if (tongue) {
+                    const button = createElementWithClassAndParent("button", buttonContainer);
+                    button.innerText = "My Tongue"
+                    button.onclick = () => {
+                        tongue = false;
+                        tellStory(story, 'tongue is');
+                    }
+                }
+
+                if (arms) {
+                    const button = createElementWithClassAndParent("button", buttonContainer);
+                    button.innerText = "My Arms"
+                    button.onclick = () => {
+                        arms = false;
+                        tellStory(story, "arms are");
+                    }
+                }
+
+                if (legs) {
+                    const button = createElementWithClassAndParent("button", buttonContainer);
+                    button.innerText = "My Legs"
+                    button.onclick = () => {
+                        legs = false;
+                        tellStory(story, "legs are");
+                    }
+                }
+
+
                 removeItemOnce(remaingStories, story)
             }
         }
+        let numDoors = 0;
 
         //the three doors aren't guaranteed to be different from each other
         //i don't care
         if (remaingStories.length > 0) {
-            doorAndStory();
+            doorAndStory("West");//yes yes yuck it up, west is real but south isn't here. ironic isn't it?
+            numDoors++;
         }
 
         if (remaingStories.length > 1) {
-            doorAndStory();
+            doorAndStory("North");
+            numDoors++;
+
         }
 
         if (remaingStories.length > 2) {
-            doorAndStory();
+            doorAndStory("East");
+            numDoors++;
+
         }
+
+        storyEle.innerHTML += `<br><br>There are ${numDoors} doors before you. Which will you choose?`
     }
 
     init();
