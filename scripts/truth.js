1c059d05487960068d86884c77f4c44eaf4b08e0
Jennifer Reynolds <jadedResearcher@gmail.com>
2026-07-11 22:25:29 -0400

    hoon password

--- scripts/truth.js
+++ scripts/truth.js
@@ -361,7 +361,7 @@ wireUpAchievement("It's Friday!!!", "the door is a jar", "In Truth, Fridays are 
 wireUpAchievement("Minotaur Fuckery", "are you sure this was here before", "In Truth, the correct action when you find a dead end in a maze is to grab a shovel and get digging, so the next Wanderer has something to find.");
 wireUpAchievement("LeeHunter Encounter", "ascended bug", "In Truth, JR delights in treating glitches and bugs as opportunity for Lore.");
 wireUpAchievement("Orchestral Piano", "time is fake", "In Truth, Zampanio has no concept of time because JR enjoys twisting and warping canon in ways that don't have time. ");
-//wireUpAchievement("Wrong", "test_password", "TBD");
+wireUpAchievement("Hoon Encounter", "cast the first stone", "In Truth, Hoon gave up her own ability to judge long ago. Now she simply allows the radio to tell her who must live and who must die..");
 //wireUpAchievement("Wrong", "test_password", "TBD");
 //wireUpAchievement("Wrong", "test_password", "TBD");
 
---
+++ secrets/cast the first stone.js
@@ -0,0 +1,75 @@
+
+//immediately envoked function does't run into namespacign issues
+(() => {
+
+    const codexURL = `http://farragofiction.com/CodexOfRuin/viewer.html?name=Hoon&data=N4IgdghgtgpiBcIASB7FYQBoQBMYGcBLAc0gBdD0EQAZAWgAY6AFAJixACdD8BragOoBBARzIALGLHzUAUgFUAIgHEAogFlVAOQAqmAAQBGQwGZMAJSGKAkgHkDxs-oBC184syODz86qE6kB1NMZnNrLQBhVSCzAGUaIXllJB1VcxiOfDIIMhlEVQANVPMtIRpPYOZ-a209L2tFPxprWJ1rCIqzIQio5jatZU7MGlsATTKhiLL1To4YAA8IAGMyABsATwB9CU4YGG3CAAc82joIxgBWfR59ACMAVwAzR5gyGBx9WE4lwlWCfA2d3W+gkMH0AHdCGAcPh9ChHvpHjkYAA6fQRCD3Cjoa6wsgoO5g+74d4QyRgfQ6OgCOjOQy4-QQVb4AlQ-SHVYQdZogxCMiM24s1ZYsGcCDQlBQADc+no5wYFwA5LCshBbr9CGRgUsUPcwG9OJ8ufpiAScChwWAUcMzpd9EtxfpzYywOsJFDiIz9MpoDBDTr7qsPjqwEsYIQAG5qwHmlEcKFZKErahIEjiTCp4jpzPZtMZtPxsBELNkag0C3DCvl8GVmvVjg5bJLcSwfUpvM5-NZru5rMcXaHXb4IhURAANT9wOrmAnnCnFdn85ri9lC8nq-BmWy6tWmq2fwjMFW1EM-femzwLyLcEQDBRp+w4PEmv2l5g1+o7Gwt05S14F5gK8SWoO8Lg4Q4mVfQD32AxAHxAQgoGITZ8G+ahxDIMhDngAB6HCkU4MVTUeQgVkoK0dSgHCdHBFAfXWWQUF4AgcNifFuBY9R0CyP1rBDfAcPENAwHVTgcBRQ4wGILcUDnADsl+E5UBxcFwzEwFd2Y2EBE1SROFudYDHxfRnxVXcS30KRbggQiIGHVsyDRHRJGBfBxBssFljDfV8CMoT7izEywXc2FaM4ZiPncsSQQJfAlk4e5bkZWEoHuZs4QRTVEU4SUTJuKBdQkZKgvtcUUQAHTASqMKw3D8Jsoj4VI7EKMlHDmBs5jOHLMhyyyHCxCOU9EHoJg2AZCA7nFHAIGIUlaKgR1ISKyashyqSIQIA0KS899HMqyrrH5G5KI5V4j1ciAXhixlDkHFBlnEfQWX0VZ0E9OzrlyfQxRwSh9AAClGlhWDoIQAEonRQAh9DAFBjrwfVCEeYF1l1YrJvKkACqLA0sbhQ0sduXZHo9LG42wChDnYRBrARPViZgR73gMYHxsW4F4ReQ1HjQHADDco8DQJyz9T9BSwAcm78ESgArGAVnwCmQCpkxqDp2VGBBsHrkRigSJhllYHdDbPsmnYmbIVmtfGyFVlWa4oFgP7kUBMgxSLXnOCga59QJSbOTAXhgVEj4cZ4w1psZTCpEOfljPcjbQRBGy5v5R4cp95OwAV3h9D1CgHbwCBxJAABfIA`;
+    const src = `<div style='background: black;color: white;font-family: Courier New;padding: 31px;'><a target='_blank' href ='${codexURL}'>Title: The Radio</a>.
+    
+
+
+    Look. I get it.
+
+    The mall is scary. (and fun!)
+
+    It's dark. (and mysterious!)
+
+    There's monsters in it. (definitely can kill you!)
+
+    We all know the "Fruit awaits us within, and that we have promised our Harvest God we will eat consume it". We all know the Scripture.
+
+    If it was easy the rewards would not be so great! Right! Camellia is always saying that!
+
+    Which is why I am...Not MAD....Just disapointed. 
+
+    That so many of you have been fraternizing with the enemy!
+
+    Guys!
+
+    It's not hard!
+
+    Everyone but us in there is an enemy! I know that's a really strong thing to say but its true okay!
+
+    Yes, even the chill lady with the radio who lets you listen to it and rest!
+
+    ESPECIALLY her!
+
+    She strangled Jeb to death last week!
+
+    Now I know the rumors: Everone is saying that she only kills "monsters".
+
+    Was Jeb a monster?
+
+    No, he was one of us!
+
+    So stop getting complacent and leave that radio lady ALONE!
+
+    Geez I swear you guys are as bad as my old coworkers! At least the Toms had an excuse for forgetting which paths in the office were deadly which days!
+
+    And YES I know if I keep adding my own comments to these reports you'll give them back to Old Susan to write but come ON! You guys don't even have footnotes in these things?
+
+    Where's the sense of confusion? The mystery? The disorientation?
+
+    Just because we worship the Harvest doesn't mean we have to be sticks in the mud, right?
+
+
+        <u>Exhibit A</u>: Hoon doesn't seem to hang EVERY mannequin she finds. Apparently even they have some type of greater or less monster rating?? Anyways if you see piles of mannequins you probably are gonna see hanging mannequins near them. Run!
+
+    <img style='width: 75%'src ="http://farragofiction.com/MallSim/images/Diorama/ParkingLotLost/Hoon/yesyesyes.jpg">
+    
+
+    <u>Exhibit B</u>: If you see hanging bodies, even if they're 'just' mannequins, RUN!! This isn't a normal safe maze, guys, I don't know how much I can emphasize that!
+    
+    <img style='width: 75%' src ='http://farragofiction.com/MallSim/images/Diorama/ParkingLotLost/Hoon/rustandbodies3.jpg'>
+    <br><br>
+      <u>Exhibit C</u>: Yes! Even bodies in the dark count! You don't have to SEE them to be in danger! The Mannequins are not the danger, okay? It's the bandaged lady who HANGS them up!!
+    
+    <img style='width: 75%' src ='http://farragofiction.com/MallSim/images/Diorama/ParkingLotLost/Hoon/highfiveinthedark.jpg'>
+
+</div>
+    `
+
+    resultsEle.innerHTML = src.split("\n").join("<br>");
+
+
+})()
+
